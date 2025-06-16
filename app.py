060eabbd66411cb186b2e15178afa31632e926ea
Yazz <itsjustmaitai@gmail.com>
2025-06-16 14:04:12 +0300

    Chnged stuff

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 from bs4 import BeautifulSoup
+import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    scraped_data=[]
+    scraped_data = []
 
-
-    if request.method =='POST':
-        url=request.method['url']
-        header={'User-Agent':'Mozilla/5.0'}
-        response =response.get(url, header==header)
-        soup=BeautifulSoup(response.text, 'html.parser')
-
+    if request.method == 'POST':
+        url = request.form['url']
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
         for link in soup.find_all('a'):
-            href=link.get('href')
-            text=link.text.strip()
+            href = link.get('href')
+            text = link.text.strip()
             if href:
-                scraped_data.append({'text':text ,'href':href})
+                scraped_data.append({'text': text, 'href': href})
 
     return render_template('index.html', data=scraped_data)
 
